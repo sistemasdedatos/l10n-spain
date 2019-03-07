a2a8587b676bc7728780f69e4bdf0de36aeda463
Jesús Ventosinos Mayor <jesus@comunitea.com>
2019-03-07 11:31:01 +0100

    Update partner.py

--- l10n_es_facturae/partner.py
+++ l10n_es_facturae/partner.py
@@ -27,7 +27,7 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     facturae = fields.Boolean('Factura electrónica')
-    organo_gestor = fields.Char('Órgano gestor', size=10)
+    organo_gestor = fields.Char('Organo gestor', size=10)
     unidad_tramitadora = fields.Char('Unidad tramitadora', size=10)
     oficina_contable = fields.Char('Oficina contable', size=10)
-    organo_proponente = fields.Char('Órgano proponente', size=10)
+    organo_proponente = fields.Char('Organo proponente', size=10)
